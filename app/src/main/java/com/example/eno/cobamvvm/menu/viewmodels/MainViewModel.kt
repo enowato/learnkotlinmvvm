77d3aff09a48350eef26970232207bbe3f1b06cd
enowato <muhammad.eno97@gmail.com>
2019-01-14 09:55:55 +0700

    converter

---
+++ app/src/main/java/com/example/eno/cobamvvm/menu/viewmodels/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.eno.cobamvvm.menu.viewmodels
+import android.app.Application
+import android.arch.lifecycle.AndroidViewModel
+
+class MainViewModel (application: Application):AndroidViewModel(application) {
+
+}
